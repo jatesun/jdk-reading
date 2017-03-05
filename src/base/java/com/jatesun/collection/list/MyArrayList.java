ae53b7bdb5f24903c9bf7fc2962c5322cd041935
jatesun <jatesun111@163.com>
2017-03-05 16:13:52 +0800

    arraylist源码（jdk1.6版本）完成

--- src/base/java/com/jatesun/collection/list/MyArrayList.java
+++ src/base/java/com/jatesun/collection/list/MyArrayList.java
@@ -10,7 +10,9 @@ import java.util.RandomAccess;
 /**  
  * @Title: MyArrayList.java
  * @Description: 内部使用数组实现list接口的集合类
+ * 	tips:
  * 		1、写代码对参数校验是必须的，也是反正NPE（null pointer exception）的好方法。
+ * 		2、写一个方法步骤基本上为：①校验参数 ②执行方法内操作 处理后续（返回结果等）
  * @author: jateSun  
  * @date: 2017年3月4日 下午6:12:17
  * @version: V1.0  
@@ -161,57 +163,34 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E>, RandomAc
 		return true;
 	}
 
-	// 在指定index插入元素
 	public void add(int index, E element) {
+		// 校验参数
 		if (index > size || index < 0) throw new IndexOutOfBoundsException("Index: " + index
 				+ ", Size: " + size);
-
-		ensureCapacity(size + 1); // Increments modCount!!
+		ensureCapacity(size + 1); // 校验是否扩容
+		// 将index以及后面的元素往后移动一个元素（操作费时）
 		System.arraycopy(elementData, index, elementData, index + 1, size - index);
-		elementData[index] = element;
+		elementData[index] = element;// 将元素插入index
 		size++;
 	}
 
-	/**
-	 * Removes the element at the specified position in this list.
-	 * Shifts any subsequent elements to the left (subtracts one from their
-	 * indices).
-	 *
-	 * @param index the index of the element to be removed
-	 * @return the element that was removed from the list
-	 * @throws IndexOutOfBoundsException {@inheritDoc}
-	 */
 	public E remove(int index) {
-		RangeCheck(index);
-
+		RangeCheck(index);// 校验index
 		modCount++;
-		E oldValue = (E) elementData[index];
-
+		E oldValue = (E) elementData[index];// 暂存删除的元素
+		// 计算移动元素个数，删除元素后面所有元素前移一位
 		int numMoved = size - index - 1;
 		if (numMoved > 0) System.arraycopy(elementData, index + 1, elementData, index, numMoved);
-		elementData[--size] = null; // Let gc do its work
-
+		elementData[--size] = null; // 置null，让gc回收
 		return oldValue;
 	}
 
-	/**
-	 * Removes the first occurrence of the specified element from this list,
-	 * if it is present.  If the list does not contain the element, it is
-	 * unchanged.  More formally, removes the element with the lowest index
-	 * <tt>i</tt> such that
-	 * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>
-	 * (if such an element exists).  Returns <tt>true</tt> if this list
-	 * contained the specified element (or equivalently, if this list
-	 * changed as a result of the call).
-	 *
-	 * @param o element to be removed from this list, if present
-	 * @return <tt>true</tt> if this list contained the specified element
-	 */
 	public boolean remove(Object o) {
+		// 分两种情况删除，null和非null
 		if (o == null) {
 			for (int index = 0; index < size; index++)
 				if (elementData[index] == null) {
-					fastRemove(index);
+					fastRemove(index);// 跟remove方法差不多，不过不校验参数，不返回删除元素
 					return true;
 				}
 		} else {
@@ -224,10 +203,6 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E>, RandomAc
 		return false;
 	}
 
-	/*
-	 * Private remove method that skips bounds checking and does not return the
-	 * value removed.
-	 */
 	private void fastRemove(int index) {
 		modCount++;
 		int numMoved = size - index - 1;
@@ -235,117 +210,54 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E>, RandomAc
 		elementData[--size] = null; // Let gc do its work
 	}
 
-	/**
-	 * Removes all of the elements from this list.  The list will
-	 * be empty after this call returns.
-	 */
 	public void clear() {
 		modCount++;
-
 		// Let gc do its work
 		for (int i = 0; i < size; i++)
 			elementData[i] = null;
-
 		size = 0;
 	}
 
-	/**
-	 * Appends all of the elements in the specified collection to the end of
-	 * this list, in the order that they are returned by the
-	 * specified collection's Iterator.  The behavior of this operation is
-	 * undefined if the specified collection is modified while the operation
-	 * is in progress.  (This implies that the behavior of this call is
-	 * undefined if the specified collection is this list, and this
-	 * list is nonempty.)
-	 *
-	 * @param c collection containing elements to be added to this list
-	 * @return <tt>true</tt> if this list changed as a result of the call
-	 * @throws NullPointerException if the specified collection is null
-	 */
 	public boolean addAll(Collection<? extends E> c) {
 		Object[] a = c.toArray();
 		int numNew = a.length;
-		ensureCapacity(size + numNew); // Increments modCount
-		System.arraycopy(a, 0, elementData, size, numNew);
+		ensureCapacity(size + numNew); // 校验是否需要扩容
+		System.arraycopy(a, 0, elementData, size, numNew);// 加入新元素
 		size += numNew;
 		return numNew != 0;
 	}
 
-	/**
-	 * Inserts all of the elements in the specified collection into this
-	 * list, starting at the specified position.  Shifts the element
-	 * currently at that position (if any) and any subsequent elements to
-	 * the right (increases their indices).  The new elements will appear
-	 * in the list in the order that they are returned by the
-	 * specified collection's iterator.
-	 *
-	 * @param index index at which to insert the first element from the
-	 *              specified collection
-	 * @param c collection containing elements to be added to this list
-	 * @return <tt>true</tt> if this list changed as a result of the call
-	 * @throws IndexOutOfBoundsException {@inheritDoc}
-	 * @throws NullPointerException if the specified collection is null
-	 */
 	public boolean addAll(int index, Collection<? extends E> c) {
+		// 校验index
 		if (index > size || index < 0) throw new IndexOutOfBoundsException("Index: " + index
 				+ ", Size: " + size);
-
+		// 跟addAll相似，不同的为从index开始添加元素
 		Object[] a = c.toArray();
 		int numNew = a.length;
-		ensureCapacity(size + numNew); // Increments modCount
-
+		ensureCapacity(size + numNew);
 		int numMoved = size - index;
 		if (numMoved > 0) System.arraycopy(elementData, index, elementData, index + numNew,
 				numMoved);
-
 		System.arraycopy(a, 0, elementData, index, numNew);
 		size += numNew;
 		return numNew != 0;
 	}
 
-	/**
-	 * Removes from this list all of the elements whose index is between
-	 * <tt>fromIndex</tt>, inclusive, and <tt>toIndex</tt>, exclusive.
-	 * Shifts any succeeding elements to the left (reduces their index).
-	 * This call shortens the list by <tt>(toIndex - fromIndex)</tt> elements.
-	 * (If <tt>toIndex==fromIndex</tt>, this operation has no effect.)
-	 *
-	 * @param fromIndex index of first element to be removed
-	 * @param toIndex index after last element to be removed
-	 * @throws IndexOutOfBoundsException if fromIndex or toIndex out of
-	 *              range (fromIndex &lt; 0 || fromIndex &gt;= size() || toIndex
-	 *              &gt; size() || toIndex &lt; fromIndex)
-	 */
 	protected void removeRange(int fromIndex, int toIndex) {
+		// 比较简单，将后面的元素想前面移动(toindex-fromindex)元素，然后将数组后面的引用置null
 		modCount++;
 		int numMoved = size - toIndex;
 		System.arraycopy(elementData, toIndex, elementData, fromIndex, numMoved);
-
-		// Let gc do its work
 		int newSize = size - (toIndex - fromIndex);
 		while (size != newSize)
 			elementData[--size] = null;
 	}
 
-	/**
-	 * Checks if the given index is in range.  If not, throws an appropriate
-	 * runtime exception.  This method does *not* check if the index is
-	 * negative: It is always used immediately prior to an array access,
-	 * which throws an ArrayIndexOutOfBoundsException if index is negative.
-	 */
 	private void RangeCheck(int index) {
 		if (index >= size) throw new IndexOutOfBoundsException("Index: " + index + ", Size: "
 				+ size);
 	}
 
-	/**
-	 * Save the state of the <tt>ArrayList</tt> instance to a stream (that
-	 * is, serialize it).
-	 *
-	 * @serialData The length of the array backing the <tt>ArrayList</tt>
-	 *             instance is emitted (int), followed by all of its elements
-	 *             (each an <tt>Object</tt>) in the proper order.
-	 */
 	private void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
 		// Write out element count, and any hidden stuff
 		int expectedModCount = modCount;
@@ -362,10 +274,6 @@ public class MyArrayList<E> extends AbstractList<E> implements List<E>, RandomAc
 
 	}
 
-	/**
-	 * Reconstitute the <tt>ArrayList</tt> instance from a stream (that is,
-	 * deserialize it).
-	 */
 	private void readObject(java.io.ObjectInputStream s) throws java.io.IOException,
 			ClassNotFoundException {
 		// Read in size, and any hidden stuff
